4f465d77bd1cab64449f544fc9b9a4eb125020e0
Ítalo Santana <techitalosantana@gmail.com>
2023-04-12 17:43:35 -0300

    fix(wip): error on webpack and babel loader running server

---
+++ packages/babel/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    presets: [
+      '@babel/preset-react',
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+      '@babel/preset-typescript',
+      '@babel/preset-flow',
+    ],
+    
+  };
