d9d34c161d128d4bb4407e1852f3066a034156c9
Ítalo Santana <techitalosantana@gmail.com>
2023-04-12 18:30:54 -0300

    fix/auth

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,16 +1,55 @@
-import Koa from 'koa';
+import Koa, { Request } from 'koa';
 import Router from 'koa-router';
 import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
 
+import GraphQLHTTP, { OptionsData } from 'koa-graphql';
+
 import koaPlayground from 'graphql-playground-middleware-koa';
+import { schema } from './modules/schema';
+import { config } from './config';
 
 const app = new Koa();
+app.use(bodyParser());
+app.use(cors());
+
 const router = new Router();
+
+const graphqlSettingsPerReq = async (req: Request): Promise<OptionsData> => {
+    return {
+      graphiql:
+        config.NODE_ENV !== 'production'
+          ? {
+              headerEditorEnabled: true,
+              shouldPersistHeaders: true,
+            }
+          : false,
+      schema,
+      pretty: true,
+      customFormatErrorFn: ({ message, locations, stack }) => {
+        /* eslint-disable no-console */
+        console.log(message);
+        console.log(locations);
+        console.log(stack);
+        /* eslint-enable no-console */
+  
+        return {
+          message,
+          locations,
+          stack,
+        };
+      },
+    };
+  };
+
+const graphqlServer = GraphQLHTTP(graphqlSettingsPerReq);
+
 
 router.get('/', async ctx => {
     ctx.body = 'Ok';
 });
+
+router.all('/graphql', graphqlServer);
 
 router.all(
     '/playground',
@@ -19,8 +58,6 @@ router.all(
     }),
 );
 
-app.use(bodyParser());
-app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
 export default app;
